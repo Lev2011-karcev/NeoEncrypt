9fe9225d8cdf9812393ee12d186f7f62d73512f5
levchik2011 <lev.kartsev.00@mail.ru>
2025-12-10 12:18:42 +0500

    shared_key

--- diffie.cpp
+++ diffie.cpp
@@ -1,6 +1,6 @@
 #include "diffie.h"
 #include <complex>
-
+#include <openssl/sha.h>
 
 
 long long real_pow(long long base, long long exp, long long mod){
@@ -19,6 +19,14 @@ long long secret_key(long long p, long long g, long long h){
     return real_pow(g, h, p);
 }
 
-long long diffie_hellman(long long p, long long a, long long b){//a - это ключ котрый получает юзер от другого, b - показатель степени это то secret key который мы  отправляли другому
-    return real_pow(a, b, p);
+long long session_key(long long p, long long a, long long b){//a - это ключ котрый получает юзер от другого, b - показатель степени это то secret key который мы  отправляли другому
+    return real_pow(a, b, p); // общий секретный ключ
+}
+
+std::vector<unsigned char> shared_key(long long ses_key){
+    std::vector<unsigned char> aes_key(32);
+    std::vector<unsigned char> buf(8);
+    for (int i = 0 ; i < 8 ;i++) buf[i] = (ses_key >> (i * 8)) & 0xFF;
+    SHA256(buf, 8, aes_key);
+    return aes_key;
 }
--- diffie.h
+++ diffie.h
@@ -1,11 +1,13 @@
 // Защита от повторного включения
 #ifndef DIFFIE_H
 #define DIFFIE_H
+#include <vector>
 
 // Объявление функции
-long long diffie_hellman(long long p, long long g, long long a, long long b);
+long long session_key(long long p, long long g, long long a, long long b);
 long long real_pow(long long base, long long exp, long long mod);
 long long secret_key(long long p, long long g, long long h);
+std::vector<unsigned char> shared_key(long long ses_key);
 #endif // DIFFIE_H
 
 //
