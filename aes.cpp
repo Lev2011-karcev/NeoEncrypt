308335805458cd9173a87a2af68ed407cf8662c8
levchik2011 <lev.kartsev.00@mail.ru>
2025-12-12 11:39:15 +0500

    AddRoundKey

--- aes.cpp
+++ aes.cpp
@@ -50,7 +50,16 @@ void ShiftRows(uint8_t state[4][4]){
     std::rotate(&state[3][0], &state[3][3], &state[3][4]);
 }// циклическитй сдвиг строк
 void MixColumns(uint8_t state[4][4]){}// перемешивание колонок через матрицу GF(2^8)
-void AddRoundKey(uint8_t state[4][4], const std::vector<uint8_t>& roundKey){}// делаем XOR
+void AddRoundKey(uint8_t state[4][4], const std::vector<uint8_t>& roundKey){
+    if (roundKey.size() > 16){
+        throw std::invalid_argument("Invalid round key size");
+    }
+    for(int i = 0 ;i < 4; i++){
+        for (int j = 0; j < 4; j++){
+            state[i][j] ^= roundKey[i * 4 + j];
+        }
+    }
+}// делаем XOR
 
 std::vector<uint8_t> aes_encrypt(const uint8_t input[16], const uint8_t key[16]){
 
